f89b3d70ac0862a1a9938c9d15cbd9a3af02c2b1
FZQA <913229100@qq.com>
2018-11-05 17:19:24 +0800

    vsm+knn

--- readfile.py
+++ readfile.py
@@ -7,7 +7,7 @@ Created on Sun Oct 14 19:42:17 2018
 """
 
 import os 
-import random
+
 dir_name=os.getcwd()
 data_dir=dir_name+'/../20news-18828'
 data_dir_list=os.listdir(data_dir)#20dir name
@@ -20,11 +20,3 @@ for dfile in data_dir_list:
 
 #print(len(list_file))
 
-list_file_train=random.sample(list_file,int(len(list_file)*0.8))
-print(list_file_train)
-
-#list_file_test=list_file-list_file_train
-
-list_file_test=[i for i in list_file if i not in list_file_train]
-
-print(list_file_test)
--- vsm.py
+++ vsm.py
@@ -19,37 +19,49 @@ import readfile
 
 
 file_word_dict={}
+
+
 #Traverse and read files
-for index,f in enumerate(readfile.list_file):
-    with open(f,'rb') as file:
-        w_list=list(file.read())
-    for ind_1,i in enumerate(w_list):
-        if not chr(i).isprintable(): #can't be printed
-            w_list[ind_1]=0x20
-    lower_sentence=(''.join(chr(i) for i in w_list)).lower()  #lower sentence
-    lower_word_list=word_tokenize(lower_sentence)
-    
-    english_lemmatizer=WordNetLemmatizer() #Part of speech reduction
-    for ind_2,i in enumerate(lower_word_list):
-        lower_word_list[ind_2]=english_lemmatizer.lemmatize(i)
-    
-    for w in lower_word_list[:]:
-        if not wordnet.synsets(w):   #not danci
-            lower_word_list.remove(w)    
-    
-    stwords=stopwords.words('english')
-    for w in lower_word_list[:]:
-        if(len(w)==i) or (w[0].isdigit()):
-            if w in stwords:
-                lower_word_list.remove(w)
-                
-    file_word_dict.setdefault(f,{})
-    
-    for word in lower_word_list:
-        if word in file_word_dict[f]:
-            file_word_dict[f][word]+=1
-        else:
-            file_word_dict[f].setdefault(word,1)
+def get_word_nubmber(list_file):
+    for index,f in enumerate(list_file):
+        with open(f,'rb') as file:
+            w_list=list(file.read())
+        for ind_1,i in enumerate(w_list):
+            if not chr(i).isprintable(): #can't be printed
+                w_list[ind_1]=0x20
+        lower_sentence=(''.join(chr(i) for i in w_list)).lower()  #lower sentence
+        lower_word_list=word_tokenize(lower_sentence)
+        
+        english_lemmatizer=WordNetLemmatizer() #Part of speech reduction
+        for ind_2,i in enumerate(lower_word_list):
+            lower_word_list[ind_2]=english_lemmatizer.lemmatize(i)
+        
+        for w in lower_word_list[:]:
+            if not wordnet.synsets(w):   #not danci
+                lower_word_list.remove(w)    
+        
+        stwords=stopwords.words('english')
+        for w in lower_word_list[:]:
+            if(len(w)==i) or (w[0].isdigit()):
+                if w in stwords:
+                    lower_word_list.remove(w)
+                    
+        file_word_dict.setdefault(f,{})
+        
+        for word in lower_word_list:
+            if word in file_word_dict[f]:
+                file_word_dict[f][word]+=1
+            else:
+                file_word_dict[f].setdefault(word,1)
+
+get_word_nubmber(readfile.list_file)
+
+
+#write file_word_dict  into file_word_dict.json
+file_word_json=json.dumps(file_word_dict)
+fw=open('file_word_dict.json','w')
+fw.write(file_word_json)
+fw.close()
 
 #print(file_word_dict)
 word_numbers={}
@@ -59,7 +71,8 @@ for file in file_word_dict:
             word_numbers[w]+=1
         else:
             word_numbers.setdefault(w,1)
-            
+
+
 temps=[]  # not in 10 - 1000
 for w in word_numbers:
     if not(10<word_numbers[w]<1000): 
@@ -71,29 +84,32 @@ for w in word_numbers:
 for w in temps:
     word_numbers.pop(w)
 
-#vsm tf-idf
+#vsm tf-idf,
+
 vsm_word=deepcopy(file_word_dict)
 idf_word={}
 
-for w in word_numbers:
-    w_f_num=0
+def com_VSM():
+    for w in word_numbers:
+        w_f_num=0
+        for file in file_word_dict:
+            if w in file_word_dict[file]:
+                w_f_num+=1
+        idf=numpy.log(len(file_word_dict)/w_f_num)
+        idf_word.setdefault(w,idf)
+        
+    alf=0.1
+    cnt=0
     for file in file_word_dict:
-        if w in file_word_dict[file]:
-            w_f_num+=1
-    idf=numpy.log(len(file_word_dict)/w_f_num)
-    idf_word.setdefault(w,idf)
-    
-alf=0.1
-cnt=0
-for file in file_word_dict:
-    cnt+=1
-    max_word_cnt=1
-    for w in file_word_dict[file]:
-        if max_word_cnt<file_word_dict[file][w]:
-            max_word_cnt=file_word_dict[file][w]
-    for w in file_word_dict[file]:
-        tf=alf+(1-alf)*(file_word_dict[file][w]/max_word_cnt)
-        vsm_word[file][w]=tf*idf_word[w]
+        cnt+=1
+        max_word_cnt=1
+        for w in file_word_dict[file]:
+            if max_word_cnt<file_word_dict[file][w]:
+                max_word_cnt=file_word_dict[file][w]
+        for w in file_word_dict[file]:
+            tf=alf+(1-alf)*(file_word_dict[file][w]/max_word_cnt)
+            vsm_word[file][w]=tf*idf_word[w]    
+
         
 vsm_json=json.dumps(vsm_word)
 vsm_file=open('vsm.json','w')
